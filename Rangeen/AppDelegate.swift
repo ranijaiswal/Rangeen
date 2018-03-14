d4f88b747f7251866f1a699403b7288f6e46144b
Rani Jaiswal <ruj96@live.com>
2018-03-13 23:36:51 -0400

    Initial Commit

---
+++ Rangeen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rangeen
+//
+//  Created by Rani Jaiswal on 3/13/18.
+//  Copyright © 2018 Rani Jaiswal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
