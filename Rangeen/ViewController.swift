c965e2cc0cb3702df43b9bdc635db132c9a3470e
Rani Jaiswal <ruj96@live.com>
2018-03-24 16:50:29 -0500

    code refactor to avoid filter being created every 10 sec

--- Rangeen/ViewController.swift
+++ Rangeen/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: NSViewController {
     @IBOutlet var imageDisplay: NSImageCell!
     @IBOutlet var colorPickerFrom: NSColorWell!
     @IBOutlet var colorPickerTo: NSColorWell!
-    
-    // from TMReplaceColorHue
-    var hueRange: Float = 60 //hue angle that we want to replace
+    var lastColorFrom: NSColor!
+    var lastColorTo: NSColor!
+    var lastCubeFilter: CIFilter!
+    var hueRange: Float = 60 //hue angle that we want to replace from TMReplaceColorHue
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,72 +44,76 @@ class ViewController: NSViewController {
     }
     
     // updates the photo displayed
+    // TODO: figure out what to do for "options"
     func updatePhoto() {
+        updateColorCube()
         let options =  CGWindowListOption(arrayLiteral: CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly)
-        let imageCG = CGWindowListCreateImage(CGRect.infinite, options, kCGNullWindowID, CGWindowImageOption.nominalResolution)!
-        let finalImage = filterImage(imageCG: imageCG)
-        imageDisplay.image = NSImage.init(cgImage: finalImage, size: NSZeroSize)
-    }
-  
-    // adapted from TMReplaceColorHue
-    // TODO: factor out the colorCube stuff so it doesn't re-happen every 10 sec unless colors changed
-    func filterImage(imageCG: CGImage) -> CGImage {
-        let ciImage = CIImage(cgImage: imageCG)
-        
-        // get hue of colorPickerFrom
-        var ptrFrom:CGFloat = 0.0
-        colorPickerFrom.color.getHue(&ptrFrom, saturation: nil, brightness: nil, alpha: nil)
-        let defaultHue = Float(ptrFrom)*360.0
-        let centerHueAngle: Float = defaultHue/360.0
-        // get hue of colorPickerTo
-        var ptr:CGFloat = 0.0
-        colorPickerTo.color.getHue(&ptr, saturation: nil, brightness: nil, alpha: nil)
-        let destCenterHueAngle: Float = Float(ptr)
-        let minHueAngle: Float = (defaultHue - hueRange/2.0) / 360
-        let maxHueAngle: Float = (defaultHue + hueRange/2.0) / 360
-        let hueAdjustment = centerHueAngle - destCenterHueAngle
-        let size = 64
-        var cubeData = [Float](repeating: 0, count: size * size * size * 4)
-        var rgb: [Float] = [0, 0, 0]
-        var hsv: (h : Float, s : Float, v : Float)
-        var newRGB: (r : Float, g : Float, b : Float)
-        var offset = 0
-        for z in 0 ..< size {
-            rgb[2] = Float(z) / Float(size) // blue value
-            for y in 0 ..< size {
-                rgb[1] = Float(y) / Float(size) // green value
-                for x in 0 ..< size {
-                    rgb[0] = Float(x) / Float(size) // red value
-                    hsv = RGBtoHSV(rgb[0], g: rgb[1], b: rgb[2])
-                    if hsv.h < minHueAngle || hsv.h > maxHueAngle {
-                        newRGB.r = rgb[0]
-                        newRGB.g = rgb[1]
-                        newRGB.b = rgb[2]
-                    } else {
-                        hsv.h = destCenterHueAngle == 1 ? 0 : hsv.h - hueAdjustment
-                        newRGB = HSVtoRGB(hsv.h, s:hsv.s, v:hsv.v)
-                    }
-                    cubeData[offset] = newRGB.r
-                    cubeData[offset+1] = newRGB.g
-                    cubeData[offset+2] = newRGB.b
-                    cubeData[offset+3] = 1.0
-                    offset += 4
-                }
-            }
-        }
-        let b = cubeData.withUnsafeBufferPointer { Data(buffer: $0) }
-        let data = b as NSData
-        let colorCube = CIFilter(name: "CIColorCube")!
-        colorCube.setValue(size, forKey: "inputCubeDimension")
-        colorCube.setValue(data, forKey: "inputCubeData")
-        colorCube.setValue(ciImage, forKey: kCIInputImageKey)
-        let outImage = colorCube.outputImage!
+        let imageOnScreen = CIImage(cgImage: CGWindowListCreateImage(CGRect.infinite, options, kCGNullWindowID, CGWindowImageOption.nominalResolution)!)
+        lastCubeFilter.setValue(imageOnScreen, forKey: kCIInputImageKey)
         let context = CIContext(options: nil)
-        let outputImageRef = context.createCGImage(outImage, from: outImage.extent)
-        return outputImageRef!
+        let filteredImage = lastCubeFilter.outputImage!
+        let finalImage = context.createCGImage(filteredImage, from: filteredImage.extent)
+        imageDisplay.image = NSImage.init(cgImage: finalImage!, size: NSZeroSize)
     }
     
-
+    // if color preferences have changed, updates colorCubeFilter
+    func updateColorCube() {
+        if (lastColorFrom != colorPickerFrom.color || lastColorTo != colorPickerTo.color) {
+            print("HERE!")
+            // get hue of colorPickerFrom
+            var ptrFrom:CGFloat = 0.0
+            colorPickerFrom.color.getHue(&ptrFrom, saturation: nil, brightness: nil, alpha: nil)
+            let defaultHue = Float(ptrFrom)*360.0
+            let centerHueAngle: Float = Float(ptrFrom)
+            // get hue of colorPickerTo
+            var ptrTo:CGFloat = 0.0
+            colorPickerTo.color.getHue(&ptrTo, saturation: nil, brightness: nil, alpha: nil)
+            let destCenterHueAngle: Float = Float(ptrTo)
+            let minHueAngle: Float = (defaultHue - hueRange/2.0) / 360
+            let maxHueAngle: Float = (defaultHue + hueRange/2.0) / 360
+            let hueAdjustment = centerHueAngle - destCenterHueAngle
+            let size = 64
+            var cubeData = [Float](repeating: 0, count: size * size * size * 4)
+            var rgb: [Float] = [0, 0, 0]
+            var hsv: (h : Float, s : Float, v : Float)
+            var newRGB: (r : Float, g : Float, b : Float)
+            var offset = 0
+            for z in 0 ..< size {
+                rgb[2] = Float(z) / Float(size) // blue value
+                for y in 0 ..< size {
+                    rgb[1] = Float(y) / Float(size) // green value
+                    for x in 0 ..< size {
+                        rgb[0] = Float(x) / Float(size) // red value
+                        hsv = RGBtoHSV(rgb[0], g: rgb[1], b: rgb[2])
+                        if hsv.h < minHueAngle || hsv.h > maxHueAngle {
+                            newRGB.r = rgb[0]
+                            newRGB.g = rgb[1]
+                            newRGB.b = rgb[2]
+                        } else {
+                            hsv.h = destCenterHueAngle == 1 ? 0 : hsv.h - hueAdjustment
+                            newRGB = HSVtoRGB(hsv.h, s:hsv.s, v:hsv.v)
+                        }
+                        cubeData[offset] = newRGB.r
+                        cubeData[offset+1] = newRGB.g
+                        cubeData[offset+2] = newRGB.b
+                        cubeData[offset+3] = 1.0
+                        offset += 4
+                    }
+                }
+            }
+            let b = cubeData.withUnsafeBufferPointer { Data(buffer: $0) }
+            let data = b as NSData
+            let colorCube = CIFilter(name: "CIColorCube")!
+            colorCube.setValue(size, forKey: "inputCubeDimension")
+            colorCube.setValue(data, forKey: "inputCubeData")
+            
+            // update lastColorFrom and To vars and current cube image
+            lastColorFrom = colorPickerFrom.color
+            lastColorTo = colorPickerTo.color
+            lastCubeFilter = colorCube
+        }
+    }
+    
     func HSVtoRGB(_ h : Float, s : Float, v : Float) -> (r : Float, g : Float, b : Float) {
         let col = NSColor(hue: CGFloat(h), saturation: CGFloat(s), brightness: CGFloat(v), alpha: 1.0)
         return (Float(col.redComponent), Float(col.greenComponent), Float(col.blueComponent))
