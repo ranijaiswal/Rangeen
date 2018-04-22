7a93e7597ca70496456c0df9e1e6fe83b7b002bb
Rani Jaiswal <ruj96@live.com>
2018-04-22 12:58:39 -0400

    changed set colors tutorial wording

--- Rangeen/Tutorial.swift
+++ Rangeen/Tutorial.swift
@@ -16,7 +16,7 @@ class Tutorial: NSViewController {
     @IBOutlet var back: NSButton?
     var welcomeText = "Welcome to Rangeen!\n\nThis application will take pictures of your screen and adjust the colors to your preferences."
     var timerText = "You can begin by specifying how often you'd like the screenshots taken through the dropdown at the top of the screen.\n\nFor example, you might want a picture taken every second as you're browsing an article, or you might a picture never to be taken unless you click Take Photo."
-    var setColorsText = "By clicking Set Colors, you can specify which colors on your screen you want to appear differently in the screenshot.\n\nFor example, if you select a Red in the From column and a Blue in the To column, every red on your screen will be shown as a blue in the screenshot.\n\nTo encompass general shades of colors, the application will adjust any color within 30 degrees in either direction of your selection on the color wheel."
+    var setColorsText = "By clicking Set Colors, you can specify which colors on your screen you want to appear differently in the screenshot.\n\nFor example, if you select a Red in the From column and a Blue in the To column, every red on your screen will be shown as a blue in the screenshot.\n\nThe application will adjust any color within 30 degrees in both directions of your selection on the color wheel to include the shades of your selected color. The application will adjust any color within 30 degrees in both directions of your selection on the color wheel to include the shades of your selected color."
     var closingText = "Thank you for using Rangeen!"
     override func viewDidLoad() {
         super.viewDidLoad()
