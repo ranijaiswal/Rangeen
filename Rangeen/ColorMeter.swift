7177261661fd5b4318c0d9ac9084b84c60223d7f
Rani Jaiswal <ruj96@live.com>
2018-04-23 04:44:31 -0400

    pixel under cursor detected

---
+++ Rangeen/ColorMeter.swift
@@ -0,0 +1,34 @@
+//
+//  ColorMeter.swift
+//  Rangeen
+//
+//  Created by Rani Jaiswal on 4/23/18.
+//  Copyright © 2018 Rani Jaiswal. All rights reserved.
+//
+
+import Cocoa
+
+class ColorMeter: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+     //   autoreleasepool {
+            // Grab the current mouse location.
+            let mouseLoc: NSPoint = NSEvent.mouseLocation()
+            // Grab the display for said mouse location.
+            var count:UInt32 = 0
+            var displayForPoint: CGDirectDisplayID = 0
+            if CGGetDisplaysWithPoint(NSPointToCGPoint(mouseLoc), 1, &displayForPoint, &count) != .success {
+                print("Oops.")
+            }
+            // Grab the color on said display at said mouse location.
+            let image = CGDisplayCreateImage(displayForPoint, rect: CGRect(x: mouseLoc.x, y: mouseLoc.y, width: 1, height: 1))
+            let bitmap = NSBitmapImageRep(cgImage: image!)
+            let color: NSColor? = bitmap.colorAt(x: 0, y: 0)
+            print("\(color ?? NSColor.clear)")
+
+        //}
+    }
+    
+}
